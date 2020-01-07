d4f880386cd073e14368169fbccdce6e20600be6
rhgkrsus1 <rhgkrsus1@naver.com>
2020-01-07 13:58:02 +0900

    master

--- test.py
+++ test.py
@@ -158,7 +158,7 @@ def main():
     PingPongThreadInstance.start()
     PingPongThreadInstance.wait_until_full_connect()
 
-    #PingPongThreadInstance.run_motor('all', 30)
+    PingPongThreadInstance.run_motor('all', 30)
     #PingPongThreadInstance.run_motor_aggregate([-30, -15]) 
     #PingPongThreadInstance._write(PingPongThreadInstance.SetSingleSteps_bytes(1, 20, 2000))
     #PingPongThreadInstance._write(PingPongThreadInstance.SetScheduledSteps_bytes(1, [60, -20], [2000, 1000]))
